565dde511e8106602548c28c04c0e89b6ce5c6a1
GowriDithi <151521773+GowriDithi@users.noreply.github.com>
2025-09-01 21:43:59 +0530

    Update eslint.config.mjs

--- apps/client-web/eslint.config.mjs
+++ apps/client-web/eslint.config.mjs
@@ -1,4 +1,8 @@
-import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";import { FlatCompat } from "@eslint/eslintrc";
 
 const compat = new FlatCompat({
     baseDirectory: import.meta.dirname,
@@ -16,3 +20,26 @@ const eslintConfig = [
 ];
 
 export default eslintConfig;
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  }
+);
